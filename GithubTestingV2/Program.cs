ab0b408b999530cf52a73d9127efa9fa5d008084
Daniel Valsby-Koch <dava@aspit.dk>
2018-08-20 09:39:50 +0200

    Added extra information to the console

--- GithubTestingV2/Program.cs
+++ GithubTestingV2/Program.cs
@@ -11,6 +11,7 @@ namespace GithubTestingV2
         static void Main(string[] args)
         {
             Console.WriteLine("Hello GitHub");
+            Console.WriteLine("Mere tekst");
         }
     }
 }
